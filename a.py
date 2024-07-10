a6bd3166b083a3fae325e42ce1583fb0dc9842e8
AngelSalgadoM <angel.salgado@alumnos.uv.cl>
2024-07-10 12:14:44 -0400

    salidas

--- a.py
+++ a.py
@@ -1,9 +1,9 @@
 import subprocess
 
-for i in range(5,25):
+for i in range(1,4):
     print(f"va en {i}")
     for j in range(1000):
-        a=f"./example --jobs 500 --rate 5 --cajas {i} --qrate 10 --fail 50 --tipoa 3 --tipob 5".split()
-        compile=subprocess.run(a,capture_output=True, text=True)
+        a=f"./simulador/example --jobs 500 --rate 5 --cajas {i} --qrate 10 --fail 50 --tipoa 3 --tipob 5".split()
+        compile=subprocess.run(a, capture_output=True, text=True)
 
         print(str(compile.stdout).strip(),file=open(f"{i}-salida.txt",'+a'))
